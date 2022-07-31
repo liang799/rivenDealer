476461beca37949e28f0108e8563b271932f8087
liang799 <71926348+liang799@users.noreply.github.com>
2022-08-01 03:19:35 +0800

    Added testing method

--- bot.py
+++ bot.py
@@ -4,3 +4,7 @@ import discord
 class Dealer(discord.Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message):
+        if message.content.startswith('99!'):
+            msg = await message.channel.send('testing123')
