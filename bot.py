3fb4e5c51154328c97910ba9f2aa8b3e21deb91a
liang799 <71926348+liang799@users.noreply.github.com>
2022-08-01 06:03:24 +0800

    Added help slash command

--- bot.py
+++ bot.py
@@ -2,9 +2,16 @@ import discord
 
 
 class MyCog(discord.Cog):
-    @discord.slash_command(name="test")
-    async def test(self, ctx):
-        await ctx.respond("TEST")
+    @discord.slash_command(name="help", description="Display the help panel")
+    async def help(self, ctx):
+        embed = discord.Embed(title='Help Panel', description='Riven Dealer has a limited vocabulary!')
+        embed.add_field(name='/help', value='Display help panel')
+        embed.add_field(name='/open', value='Open a Riven Mod')
+        embed.add_field(name='/bet', value='Place a bet')
+        embed.add_field(name='/forfeit', value='Forfeit placed bet')
+        embed.add_field(name='/status', value='Check placed bets')
+        embed.add_field(name='/reveal', value='Reveal the Riven Mod')
+        await ctx.respond(embed=embed)  # Send the embed with some text
 
 
 class Dealer(discord.Bot):
