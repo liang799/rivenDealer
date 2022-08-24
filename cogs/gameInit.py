b6d8c4c61200ee61af80cdb83633515007c6d6a0
liang799 <71926348+liang799@users.noreply.github.com>
2022-08-25 01:57:55 +0800

    added another server id

---
+++ cogs/gameInit.py
@@ -0,0 +1,27 @@
+import os
+import discord
+
+from views.dropdown import RivenSelector
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+class GameInit(discord.Cog):
+    @discord.slash_command(name="open", description="Open a Riven Mod")
+    async def open(self, ctx):
+        await ctx.respond("Initiate a betting game!", view=RivenSelector(timeout=30))
+
+    # @discord.slash_command(name="reveal", description="Supabase?")
+    # async def open(self, ctx):
+    #     res = supabase.table("primary_weapons").select("*").eq('index', 0).execute()
+    #     data = res.data
+    #     await ctx.respond(data[0]['weapon'] + " " + data[0]['tier'])
+
+
+def setup(bot):  # this is called by Pycord to setup the cog
+    bot.add_cog(GameInit(bot))  # add the cog to the bot
--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from bot import Dealer
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = Dealer(debug_guilds=[1003358734841544734])
+client = Dealer(debug_guilds=[1003358734841544734, 1012051832987734127])
 
 client.run(TOKEN)
