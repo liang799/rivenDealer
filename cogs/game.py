5d8988024fd9e0b1958e8608aeeeb60ac217995f
liang799 <71926348+liang799@users.noreply.github.com>
2022-08-28 16:49:48 +0800

    Changed /open to use Options

--- cogs/game.py
+++ cogs/game.py
@@ -1,25 +1,33 @@
 import discord
-
-from views.dropdown import RivenSelector
+from discord import Option
 from database.supaHelper import Helper
 
 
 class Game(discord.Cog):
     def __init__(self, bot):
-        self.rivenPlayer = None
+        self.rivenPlayer = ""
+        self.tableName = ""
         self.bot = bot
 
     @discord.slash_command(name="open", description="Open a Riven Mod")
-    async def open(self, ctx):
+    async def open(self, ctx,
+                   riven: Option(str, "Select Riven Mod Type", choices=["Melee", "Rifle", "Pistol"], required=True)):
         self.rivenPlayer = str(ctx.author)
-        await ctx.response.send_message("Initiate a betting game!", view=RivenSelector(), ephemeral=True)
+        Helper.signUp(self.rivenPlayer)  # try to sign up
+        if riven == "Melee":
+            self.tableName = "melee_weapons"
+        if riven == "Rifle":
+            self.tableName = "primary_weapons"
+        if riven == "Pistol":
+            self.tableName = "secondary_weapons"
+        await ctx.response.send_message(f"<@{ctx.author.id}> is currently opening a **{riven} Riven Mod**")
 
     @discord.slash_command(name="reveal", description="Supabase?")
     async def reveal(self, ctx):
-        if self.rivenPlayer is None:
+        if self.rivenPlayer == "":
             await ctx.response.send_message("Please use `/open` to open a riven mod", ephemeral=True)
         elif str(ctx.author) == self.rivenPlayer:
-            self.rivenPlayer = None
+            self.rivenPlayer = ""
             weapon: str = Helper.getMeleeTier("kuva zarr")
             await ctx.respond(weapon)
         else:
@@ -30,6 +38,13 @@ class Game(discord.Cog):
         Helper.signUp(self.rivenPlayer)
         await ctx.response.send_message(self.rivenPlayer)
 
+    @discord.slash_command(name="options", description="slash?")
+    async def options(self, ctx, test: Option(str, "reaction you want", choices=["Hi", "image"], required=True)):
+        if test == "Hi":
+            await ctx.response.send_message("sjdfld")
+        if test == "image":
+            await ctx.response.send_message("ajksdjjkkkkkkkk")
 
-def setup(bot):  # this is called by Pycord to setup the cog
+
+def setup(bot):  # this is called by Pycord to set up the cog
     bot.add_cog(Game(bot))  # add the cog to the bot
