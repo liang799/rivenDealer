06307863fa83c9a8655c123cc8e6041d64e85335
liang799 <71926348+liang799@users.noreply.github.com>
2022-08-28 19:26:53 +0800

    updated help panel

--- cogs/help.py
+++ cogs/help.py
@@ -5,11 +5,11 @@ class HelpPanel(discord.Cog):
     @discord.slash_command(name="help", description="Display the help panel")
     async def help(self, ctx):
         embed = discord.Embed(title='Help Panel', description='Riven Dealer has a limited vocabulary!')
-        embed.add_field(name='/help', value='Display help panel')
+        embed.add_field(name='/help', value='Handy cmds')
+        embed.add_field(name='/register', value='Register to start')
         embed.add_field(name='/open', value='Open a Riven Mod')
         embed.add_field(name='/bet', value='Place a bet')
         embed.add_field(name='/forfeit', value='Forfeit placed bet')
-        embed.add_field(name='/status', value='Check placed bets')
         embed.add_field(name='/reveal', value='Reveal the Riven Mod')
         await ctx.respond(embed=embed)  # Send the embed with some text
 
